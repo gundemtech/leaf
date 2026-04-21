7f034e290a56ebf3c96420848550a1b92e4502bc
demoffsrmain <ustimdima2002@gmail.com>
2026-04-21 15:37:14 +0200

    Initial Commit

---
+++ LeafControl/LeafControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafControlApp.swift
+//  LeafControl
+//
+//  Created by Dmitry Demidov on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct LeafControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
