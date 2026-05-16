f458b61d8e61a3474e2ac75b62603ccfb5414b06
demoffsrmain <ustimdima2002@gmail.com>
2026-05-16 12:09:46 +0200

    feat(track-6-P3): URLGranularity + DomainAllowListReader + 8 unit tests

---
+++ Packages/LeafCore/Tests/LeafCoreTests/BrowserAllowListFilterTests.swift
@@ -0,0 +1,45 @@
+// Packages/LeafCore/Tests/LeafCoreTests/BrowserAllowListFilterTests.swift
+import XCTest
+@testable import LeafCore
+
+final class BrowserAllowListFilterTests: XCTestCase {
+    func testExtractDomainSimple() {
+        XCTAssertEqual(extractDomain("https://github.com/foo/bar"), "github.com")
+    }
+
+    func testExtractDomainLowercased() {
+        XCTAssertEqual(extractDomain("https://GITHUB.com/X"), "github.com")
+    }
+
+    func testExtractDomainEmptyOnNonStandardScheme() {
+        XCTAssertEqual(extractDomain("about:blank"), "")
+        XCTAssertEqual(extractDomain("chrome://settings"), "")
+    }
+
+    func testApplyGranularityFullUrlPassthrough() {
+        XCTAssertEqual(applyGranularity("https://github.com/foo?q=1", .fullUrl),
+                       "https://github.com/foo?q=1")
+    }
+
+    func testApplyGranularityPathStrippedDropsQuery() {
+        XCTAssertEqual(applyGranularity("https://github.com/foo?q=1", .pathStripped),
+                       "github.com/foo")
+    }
+
+    func testApplyGranularityDomainOnly() {
+        XCTAssertEqual(applyGranularity("https://github.com/foo/bar?q=1", .domainOnly),
+                       "github.com")
+    }
+
+    func testInMemoryReaderDefaultDomainOnly() {
+        let reader = InMemoryDomainAllowListReader(rules: [:])
+        XCTAssertEqual(reader.granularity(for: "github.com"), .domainOnly)
+    }
+
+    func testInMemoryReaderReturnsRegisteredGranularity() {
+        let reader = InMemoryDomainAllowListReader(rules: ["github.com": .fullUrl])
+        XCTAssertEqual(reader.granularity(for: "github.com"), .fullUrl)
+        XCTAssertEqual(reader.granularity(for: "api.github.com"), .domainOnly,
+                       "subdomain match is exact-only")
+    }
+}
